c995dbf36a5d3c31a688dcb60fa893b177fba811
visan <alexandruvisan44@yahoo.com>
2023-11-30 12:25:33 +0200

    Fix ex2 and ex3 in lab7

--- Lab7/ex2.py
+++ Lab7/ex2.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Compute the snr of the image,
-def snr(image, noise):
-    p_signal = np.linalg.norm(image) ** 2
-    p_noise = np.linalg.norm(noise) ** 2
-    return p_signal/p_noise 
+def snr(image):
+    a = np.mean(image)
+    b = np.std(image)
+    return a/b
 
 
 def filter_freqs(Y, ratio, width, height):
@@ -18,7 +18,7 @@ def filter_freqs(Y, ratio, width, height):
 
 
 if __name__ == "__main__":
-    fig, axs = plt.subplots(6,2, figsize=(10,10))
+    fig, axs = plt.subplots(5,2, figsize=(10,10))
     fig.tight_layout(h_pad=2)
 
     # Loading the racoon image.
@@ -31,30 +31,16 @@ if __name__ == "__main__":
     Y = np.fft.fft2(X)
     axs[0,1].set_title("FFT 2d of the original image")
     axs[0,1].imshow(20*np.log10(abs(Y)))
-
-    # Generating the noisy image. 
-    pixel_noise = 200
-    noise = np.random.randint(-pixel_noise, high=pixel_noise+1, size=X.shape)
-    X_noisy = X + noise
-    axs[1,0].set_title(f"Noisy image, SNR = {snr(X_noisy, noise)}")
-    axs[1,0].imshow(X_noisy, cmap=plt.cm.gray)
-
-    # Generating the fft2d of the noisy image.
-    Y_noisy = np.fft.fft2(X_noisy)
-    axs[1,1].set_title("FFT 2d of the noisy image")
-    axs[1,1].imshow(20*np.log10(abs(Y_noisy)))
-
-    # Removing high frequencies in the noisy image.
+    
+    # Removing high frequencies from the original image.
     for i, freq_cutoff in enumerate([0.4, 0.1, 0.05, 0.02]):
-        Y_cleared = filter_freqs(Y_noisy, freq_cutoff, width, height) 
+        Y_cleared = filter_freqs(Y, freq_cutoff, width, height) 
         X_cleared = np.fft.ifft2(Y_cleared).real
 
-        title = f"SNR = {snr(X_cleared, noise)}"
-        if i == 2:
-            title += " (optimal)"
-        axs[2+i,0].set_title(title)
-        axs[2+i,0].imshow(X_cleared, cmap=plt.cm.gray)
-        axs[2+i,1].set_title(f"Cut FFT 2d, ratio = {freq_cutoff}")
-        axs[2+i,1].imshow(20*np.log10(abs(Y_cleared)) )
+        title = f"SNR = {snr(X_cleared)}"
+        axs[1+i,0].set_title(title)
+        axs[1+i,0].imshow(X_cleared, cmap=plt.cm.gray)
+        axs[1+i,1].set_title(f"Cut FFT 2d, ratio = {freq_cutoff}")
+        axs[1+i,1].imshow(20*np.log10(abs(Y_cleared)) )
     plt.show()
 
--- Lab7/ex3.py
+++ Lab7/ex3.py
@@ -4,26 +4,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import ex2
 
+if __name__ == "__main__":
+    fig, axs = plt.subplots(6,2, figsize=(10,10))
+    fig.tight_layout(h_pad=2)
 
-# Loading image and generating noisy image like in ex2.
-X = misc.face(gray=True)
-height, width = X.shape
-pixel_noise = 200
-noise = np.random.randint(-pixel_noise, high=pixel_noise+1, size=X.shape)
-X_noisy = X + noise
-Y_noisy = np.fft.fft2(X_noisy)
+    # Loading the racoon image.
+    X = misc.face(gray=True)
+    height ,width = X.shape 
+    axs[0,0].set_title("Original image")
+    axs[0,0].imshow(X, cmap=plt.cm.gray)
 
-# Denoising the image with the optimal param.
-optimal = 0.05
-Y_cleared = ex2.filter_freqs(Y_noisy, optimal,  width, height)
-X_cleared = np.fft.ifft2(Y_cleared).real
+    # Computing the fft2d of the original image.
+    Y = np.fft.fft2(X)
+    axs[0,1].set_title("FFT 2d of the original image")
+    axs[0,1].imshow(20*np.log10(abs(Y)))
 
-fig, axs = plt.subplots(2, figsize=(10,10))
+    # Generating the noisy image. 
+    pixel_noise = 200
+    noise = np.random.randint(-pixel_noise, high=pixel_noise+1, size=X.shape)
+    X_noisy = X + noise
+    axs[1,0].set_title(f"Noisy image, SNR = {ex2.snr(X_noisy)}")
+    axs[1,0].imshow(X_noisy, cmap=plt.cm.gray)
 
-axs[0].imshow(X_cleared, cmap = plt.cm.gray)
-axs[1].imshow(X_cleared-noise, cmap = plt.cm.gray)
-plt.show()
+    # Generating the fft2d of the noisy image.
+    Y_noisy = np.fft.fft2(X_noisy)
+    axs[1,1].set_title("FFT 2d of the noisy image")
+    axs[1,1].imshow(20*np.log10(abs(Y_noisy)))
 
+    # Removing high frequencies in the noisy image.
+    for i, freq_cutoff in enumerate([0.4, 0.1, 0.05, 0.02]):
+        Y_cleared = ex2.filter_freqs(Y_noisy, freq_cutoff, width, height) 
+        X_cleared = np.fft.ifft2(Y_cleared).real
 
-print(ex2.snr(X_cleared, noise))
-print(ex2.snr(X_cleared-noise, noise))
+        title = f"SNR = {ex2.snr(X_cleared)}"
+        if i == 2:
+            title += " (optimal)"
+        axs[2+i,0].set_title(title)
+        axs[2+i,0].imshow(X_cleared, cmap=plt.cm.gray)
+        axs[2+i,1].set_title(f"Cut FFT 2d, ratio = {freq_cutoff}")
+        axs[2+i,1].imshow(20*np.log10(abs(Y_cleared)) )
+    plt.show()
+
