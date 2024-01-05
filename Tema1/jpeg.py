5df85bd26c3bb1e4bee06566ec6a39b8028b9645
visan <alexandruvisan44@yahoo.com>
2024-01-05 21:04:16 +0200

    Implement decompression

--- Tema1/jpeg.py
+++ Tema1/jpeg.py
@@ -27,6 +27,7 @@ def to_YCrCb(image):
 # Applies dct and then coefficient quantization over a 8*8 block.
 # Returns a 8*8 block of np.int32.
 def quantize_block(block, debug=False):
+    assert type(block) == np.ndarray
     assert block.shape == (8, 8)
 
     y = dctn(block)
@@ -34,8 +35,7 @@ def quantize_block(block, debug=False):
     y_q = np.round(y / Q_MATRIX).astype(np.int32)
 
     if debug:
-        print(y_q)
-        block_q = idctn(y_q)
+        block_q = np.round(idctn(y_q * Q_MATRIX))
         # To see the compression result side-by-side:
         fig, axes = plt.subplots(2, 2)
         fig.tight_layout()
@@ -53,6 +53,12 @@ def quantize_block(block, debug=False):
     return y_q
 
 
+def dequantize_block(block):
+    assert type(block) == np.ndarray
+    assert block.shape == (8, 8)
+    return np.round(idctn(block * Q_MATRIX))
+
+
 # Compress a 8*8 block into a byte array.
 def compress_block(block):
     assert block.shape == (8, 8)
@@ -68,21 +74,27 @@ def decompress_block(data):
 
 
 class CompressedGrayscaleImage:
-    def __init__(self, height, width, blocks):
+    def __init__(self, height, width, padded_height, padded_width, blocks):
         self.height = height
         self.width = width
+        self.padded_height = padded_height
+        self.padded_width = padded_width
         self.blocks = blocks
 
 
 # Compresses a grayscale image using the JPEG algorithm.
 def compress_grayscale(image):
+    assert type(image) == np.ndarray
     assert len(image.shape) == 2
 
     height, width = image.shape
     # Pad the image so that we can split it into
     # blocks of 8x8
     pad_height, pad_width = height % 8, width % 8
-    image = np.pad(image, ((0, pad_height), (0, pad_width)), mode="edge")
+    if pad_height != 0:
+        image = np.pad(image, ((0, 8 - pad_height), (0, 0)), mode="edge")
+    if pad_width != 0:
+        image = np.pad(image, ((0, 0), (0, 8 - pad_width)), mode="edge")
 
     new_height, new_width = image.shape
     blocks = []
@@ -95,11 +107,36 @@ def compress_grayscale(image):
             block_data = compress_block(quantized_block)
             blocks.append(block_data)
 
-    return CompressedGrayscaleImage(height, width, blocks)
+    return CompressedGrayscaleImage(height, width, new_height, new_width, blocks)
+
+
+def decompress_grayscale(image):
+    assert type(image) == CompressedGrayscaleImage
+    decompressed = np.zeros((image.padded_height, image.padded_width))
+    current_block = 0
+
+    for y in range(0, image.padded_height, BLOCK_SIZE):
+        for x in range(0, image.padded_width, BLOCK_SIZE):
+            block_data = image.blocks[current_block]
+            quantized_block = decompress_block(block_data)
+            block = dequantize_block(quantized_block)
+            decompressed[y : y + BLOCK_SIZE, x : x + BLOCK_SIZE] = block
+            current_block += 1
+    return decompressed[: image.height, : image.width]
 
 
 image = misc.face()
 image = to_YCrCb(image)
 
-compressed = compress_grayscale(image[:, :, 0])
-print(compressed)
+channel1 = image[:, :, 0]
+compressed = compress_grayscale(channel1)
+decompressed = decompress_grayscale(compressed)
+
+
+fig, axes = plt.subplots(1, 2)
+axes[0].imshow(channel1)
+axes[1].imshow(decompressed)
+plt.show()
+
+print(decompressed.shape)
+print(((channel1 - decompressed) ** 2).mean())
