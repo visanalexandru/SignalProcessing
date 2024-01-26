29b518a80e306347a0e7a949ab26ec44dd0e6022
visan <alexandruvisan44@yahoo.com>
2024-01-26 13:23:35 +0200

    Fix ex1.py

--- Lab10/ex1.py
+++ Lab10/ex1.py
@@ -1,91 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sample_multivariate_gaussian(mean_vector, covariance_matrix):
-    n = np.random.normal(size=len(mean_vector))
-    u, s, vh = np.linalg.svd(covariance_matrix)
 
-    s = np.diag(np.sqrt(s))
-    result = np.matmul(u,s) 
-    result = np.matmul(result, n) 
-    result += mean_vector
+def sample_gaussian(mean, variance, size):
+    stdev = np.sqrt(variance)
+    return np.random.normal(mean, stdev, size=size)
 
-    return result
+def sample_multivariate_gaussian(mean_vector, variance_matrix):
+    dim = len(mean_vector)
+    assert variance_matrix.shape == (dim, dim)
 
+    u, s, _ = np.linalg.svd(variance_matrix)
+    s = np.diag(s)
+
+    n = np.random.normal(0, 1, size=dim)
+    x = np.matmul(u, np.sqrt(s))
+    x = np.matmul(x, n)
+    return x + mean_vector
 
 def graph_univariate_gauss():
-    mean = 3
-    stdev = 1
-    num_samples = 1000
-    samples = np.random.normal(mean, stdev, size=num_samples) 
-    plt.hist(samples, bins=50)
+    samples = sample_gaussian(0, 4, 10000)
+    plt.title("Univariate normal distribution")
+    plt.hist(samples, bins=100, density=True)
     plt.show()
-
-graph_univariate_gauss()
 
 def graph_multivariate_gauss():
-    mean_vector = [0,0]
-    mat = np.array([[1, 3/5], [3/5, 2]]) 
+    mean_vector = np.array([0,0])
+    covariance_matrix = np.array([[1, 3/5], [3/5, 2]])
+    samples = np.array([sample_multivariate_gaussian(mean_vector, covariance_matrix) for _ in range(1000)])
 
-    num_samples = 1000
-    samples = [sample_multivariate_gaussian(mean_vector, mat) for x in range(num_samples)]
-
-    samples_y = [sample[0] for sample in samples]
-    samples_x = [sample[1] for sample in samples]
-
+    plt.title("2d normal distribution")
     plt.axis('equal')
-    plt.scatter(samples_x, samples_y)
+    plt.scatter(samples[:, 0], samples[:, 1])
     plt.show()
 
-
-graph_multivariate_gauss()
-
-
-def graph_square_exp():
-    domain = np.linspace(0,1, 100) 
-    alpha = 0.01
-
-    mean = np.zeros(len(domain))
-    covariance = np.zeros((len(domain), len(domain)))
-
-    for a in range(len(domain)):
-        for b in range(len(domain)):
-            x = domain[a]
-            y = domain[b]
-
-            covariance[a][b] = np.exp(-alpha* np.abs(x-y)) 
-
-    samples = sample_multivariate_gaussian(mean, covariance)
-
-    plt.plot(samples)
-    plt.show()
-
-def graph_simmetrical():
-    domain = np.linspace(-5,5,100) 
-    alpha = 1 
-
-    mean = np.zeros(len(domain))
-    covariance = np.zeros((len(domain), len(domain)))
-
-    for a in range(len(domain)):
-        for b in range(len(domain)):
-            x = domain[a]
-            y = domain[b]
-
-            diff = abs(x-y)
-            sum = abs(x+y)
-
-            m = min(diff, sum)
-            covariance[a][b] = np.exp(-alpha * m**2) 
-
-    samples = sample_multivariate_gaussian(mean, covariance)
-
-    plt.plot(domain,samples.T)
-    plt.show()
-
-
-
-
-graph_square_exp()
-graph_simmetrical()
-    
+if __name__ =="__main__":
+    graph_univariate_gauss()
+    graph_multivariate_gauss()
